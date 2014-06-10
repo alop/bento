aed59f2a4a8c9d0aef31010d8a0d06377b0c89a0
Abel Lopez <abelopez@cisco.com>
2014-06-10 16:36:53 -0700

    Initial change to use qemu
    Initial change from VMWare to qemu.
    Changed all vagrant users to root

--- packer/scripts/common/sshd.sh
+++ packer/scripts/common/sshd.sh
@@ -2,3 +2,5 @@
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
 echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+echo "PermitRoot yes" >> /etc/ssh/sshd_config
